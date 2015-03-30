dcec089bac8325174ff64512d66076d184423ba3
YanagiEiichi <admin@web-tinker.com>
2015-03-30 11:04:06 +0800

    Fix root domain getting bug

--- xlocalstorage.js
+++ xlocalstorage.js
@@ -1,8 +1,25 @@
 void function() {
   var NAME = 'xLocalStorage';
   var heap = [ null ];
-  // TODO: This regexp can't support extension locale domain such as *.com.xx
-  var root = location.protocol + '//' + location.host.replace(/^.*?(?=[^.\d]+\.[^.\d]+$)/, '');
+
+  // REFERENCE: https://github.com/YanagiEiichi/getrootdomain.js 
+  var root = location.protocol + '//' + function() {
+    // Create a random stream
+    var rnd = '';
+    while(rnd.length < 32) rnd += Math.floor(Math.random() * 2821109907456).toString(36);
+    rnd = rnd.slice(0, 32);
+    // Split current host
+    var chips = location.host.split('.');
+    // Try to write cookie
+    var result = chips.pop();
+    while(!~document.cookie.indexOf(rnd) && chips.length) {
+      result = chips.pop() + '.' + result;
+      document.cookie = rnd + '=; Domain=' + result;
+    }
+    // Remove cookie
+    document.cookie = rnd + '=; Domain=' + result + '; Expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    return result;
+  }();
 
   // Install iframe to document head
   var head = document.documentElement.firstChild;
